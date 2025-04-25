53d0a6f81594e87560f6041b2625374e17eea7e6
navjack <jack.mangano@gmail.com>
2025-04-25 15:26:57 -0400

    Initial Commit

---
+++ Menu Bar TerminalTests/Menu_Bar_TerminalTests.swift
@@ -0,0 +1,17 @@
+//
+//  Menu_Bar_TerminalTests.swift
+//  Menu Bar TerminalTests
+//
+//  Created by Jack Mangano on 4/25/25.
+//
+
+import Testing
+@testable import Menu_Bar_Terminal
+
+struct Menu_Bar_TerminalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
